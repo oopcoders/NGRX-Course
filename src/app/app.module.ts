7694f0499849592f33b584b1d87c2452504b83d0
Michal Harrison <mlh1477@gmail.com>
2020-07-03 11:25:54 -0400

    8. Install @ngrx-store-devtools library and Chrome Redux Extension

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CartModule } from './modules/cart/cart.module';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -43,6 +45,7 @@ import { StoreModule } from '@ngrx/store';
     BsDropdownModule.forRoot(),
     ModalModule.forRoot(),
     StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
