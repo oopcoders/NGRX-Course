4223d3f5da8829af7d5cc710aca4d158ef4932ba
Michal Harrison <mlh1477@gmail.com>
2020-07-22 08:52:42 -0400

    19.Begin - Refactor Login Modal

--- src/app/modules/auth/auth-links/auth-links.component.ts
+++ src/app/modules/auth/auth-links/auth-links.component.ts
@@ -5,10 +5,8 @@ import { MockApiCartService } from '../../cart/resources/mock-api-cart.service';
 import { Router } from '@angular/router';
 import { User } from '../resources/auth';
 import * as fromAuthModels from '../resources/auth';
-import { Observable } from 'rxjs';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { NgForm } from '@angular/forms';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { ModalService } from '../resources/modal.service';
+import { LoginModalComponent } from '../login-modal/login-modal.component';
 
 @Component({
   selector: 'app-auth-links',
@@ -17,17 +15,13 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class AuthLinksComponent implements OnInit {
   user: User;
-  user$: Observable<User>;
-  modalRef: BsModalRef;
 
   constructor(
     public authService: AuthService,
     private alertService: AlertService,
     private cartService: MockApiCartService,
     private router: Router,
-    private modalService: BsModalService,
-    private spinner: NgxSpinnerService,
-    private route: Router
+    private modalService: ModalService
   ) {}
 
   ngOnInit(): void {
@@ -55,51 +49,7 @@ export class AuthLinksComponent implements OnInit {
     }, 2000);
   }
 
-  updateShoppingCart(userid) {
-    const observer = {
-      next: (cartlist) => {
-        this.cartService.updatedCartSelection(cartlist);
-      },
-      error: (err) => console.error(err),
-    };
-    this.cartService.getCartByUserId(userid).subscribe(observer);
-  }
-
-  onSubmit(f: NgForm) {
-    this.spinner.show();
-    this.alertService.info('Checking your information...');
-    const observer = {
-      next: (user) => {
-        this.user = user;
-
-        this.updateShoppingCart(this.user.id);
-
-        this.authService.updatedUserSelection(this.user);
-        this.modalRef.hide();
-        setTimeout(() => {
-          /** spinner ends after 5 seconds */
-          this.spinner.hide();
-          this.alertService.success(
-            'Welcome Back ' + this.user.username + ' !'
-          );
-          this.route.navigate(['/shopping/products']);
-        }, 1000);
-      },
-      error: (err) => {
-        this.alertService.danger('Unable to login');
-        this.spinner.hide();
-      },
-    };
-    this.authService
-      .login(f.value.username, f.value.password)
-      .subscribe(observer);
-  }
-
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
-  }
-
-  cancel(): void {
-    this.modalRef.hide();
+  openModal() {
+    this.modalService.show(LoginModalComponent);
   }
 }
---
+++ src/app/modules/auth/login-modal/login-modal.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AlertService } from 'ngx-alerts';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
+import { AuthService } from '../resources/auth.service';
+import { User } from '../resources/auth';
+import { Observable } from 'rxjs';
+import { MockApiCartService } from '../../cart/resources/mock-api-cart.service';
+import { ModalService } from '../resources/modal.service';
+
+@Component({
+  selector: 'app-login-modal',
+  templateUrl: './login-modal.component.html',
+  styleUrls: ['./login-modal.component.scss'],
+})
+export class LoginModalComponent implements OnInit {
+  user: User;
+  user$: Observable<User>;
+  constructor(
+    private alertService: AlertService,
+    private spinner: NgxSpinnerService,
+    private route: Router,
+    public authService: AuthService,
+    private cartService: MockApiCartService,
+    private modalService: ModalService
+  ) {}
+
+  ngOnInit(): void {}
+
+  updateShoppingCart(userid) {
+    const observer = {
+      next: (cartlist) => {
+        this.cartService.updatedCartSelection(cartlist);
+      },
+      error: (err) => console.error(err),
+    };
+    this.cartService.getCartByUserId(userid).subscribe(observer);
+  }
+
+  onSubmit(f: NgForm) {
+    this.spinner.show();
+    this.alertService.info('Checking your information...');
+    const observer = {
+      next: (user) => {
+        this.user = user;
+
+        this.updateShoppingCart(this.user.id);
+
+        this.authService.updatedUserSelection(this.user);
+        this.modalService.hide();
+        setTimeout(() => {
+          /** spinner ends after 5 seconds */
+          this.spinner.hide();
+          this.alertService.success(
+            'Welcome Back ' + this.user.username + ' !'
+          );
+          this.route.navigate(['/shopping/products']);
+        }, 1000);
+      },
+      error: (err) => {
+        this.alertService.danger('Unable to login');
+        this.spinner.hide();
+      },
+    };
+    this.authService
+      .login(f.value.username, f.value.password)
+      .subscribe(observer);
+  }
+
+  cancel(): void {
+    this.modalService.hide();
+  }
+}
